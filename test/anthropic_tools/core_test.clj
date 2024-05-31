7702286ac4e4992cc97f2806b69b41af32aff41e
Jason Walsh <j@wal.sh>
2024-05-31 11:08:14 -0400

    `lein new anthropic-tools`

---
+++ test/anthropic_tools/core_test.clj
@@ -0,0 +1,7 @@
+(ns anthropic-tools.core-test
+  (:require [clojure.test :refer :all]
+            [anthropic-tools.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
